653e73db0c39141f04d8bd0935554e26b73d92e1
Galiullin Nikolai <galiullinnikolai@gmail.com>
2016-03-08 14:52:59 +0200

    sdfsagdsfhgdyt
    mnogo klassov

---
+++ Neuro/Neuro/Neuron.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neuro
+{
+    class Neuron
+    {
+        Neuron Prev;
+        Neuron[] Next;
+        double SignalSum = 0;
+        double Weight;
+        public Neuron(Neuron tPrev, params Neuron[] tNext)
+        {
+            Prev = tPrev;
+            Next = tNext;
+        }
+        public Neuron(params Neuron[] tNext)
+        {
+            Next = tNext;
+        }
+    }
+}
